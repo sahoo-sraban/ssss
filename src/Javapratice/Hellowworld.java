58911ce48a8a54222f2b0b4c17cc4d138cae5314
sraba <sraba@LAPTOP-VGJFC243>
2021-09-30 19:29:27 +0530

    first commit

---
+++ src/Javapratice/Hellowworld.java
@@ -0,0 +1,12 @@
+package Javapratice;
+
+public class Hellowworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//@Test
+		
+
+	}
+
+}
---
+++ src/Javapratice/ssss.java
@@ -0,0 +1,10 @@
+package Javapratice;
+
+public class ssss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
